ad06289c155715403cad238fb92cb424687f317d
teguhmr <teguhmr16@gmail.com>
2024-08-30 14:09:04 +0700

    add login feature

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bengkel A3R Team</title>
+    <link rel="stylesheet" href="login-style.css" />
+  </head>
+  <body>
+    <!-- Include the login.html content here -->
+    <div>
+      <!-- You can include additional content here, before or after the login form -->
+      <?php include 'login.html'; ?>
+    </div>
+  </body>
+</html>
