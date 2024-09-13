1bb2436787694cbd2bc7c88fa761ccebda3f1573
AlifAhmadMudzaki <alifahmadmudzaki@gmail.com>
2024-09-13 17:37:29 +0700

    add file admin

---
+++ php/admin_header.php
@@ -0,0 +1,18 @@
+<!-- header.php -->
+<header>
+  <div class="navbar">
+    <div class="logo">Bengkel A3R Team</div>
+    <div class="nav-links">
+          <a href="admin_home.php"><img src="../images/home.png" alt="Home" /></a>
+          <a href="admin_pelanggan.php"><img src="../images/pelanggan.png" alt="Pelanggan" /></a>
+          <a href="admin_kendaraan.php"><img src="../images/kendaraan.png" alt="kendaraan" /></a>
+          <a href="admin_sparepart.php"><img src="../images/sparepart.png" alt="Sparepart" /></a>
+          <a href="admin_supplier.php"><img src="../images/supplier.png" alt="Supplier" /></a>
+          <a href="admin_penjualan.php"><img src="../images/penjualan.png" alt="Penjualan" /></a>
+      <a href="../php/logout.php" class="logout">Log Out</a>
+    </div>
+  </div>
+</header>
+
+<!-- You can add CSS styles here or in a separate CSS file -->
+<link rel="stylesheet" href="../css/navbar-style.css" />
---
+++ php/admin_penjualan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin Penjualan - Bengkel A3R Team</title>
+    <link rel="stylesheet" href="../css/customer-style.css" />
+  </head>
+  <body>
+    <!-- Include header and navbar -->
+    <?php include 'admin_header.php'; ?>
+
+    <!-- Main content specific to the customer home page -->
+    <main>
+      <div class="welcome-section">
+        <img src="../images/logo-bengkel.png" alt="Mechanic Image" />
+        <h1>PENJUALAN</h1>
+      </div>
+    </main>
+  </body>
+</html>
